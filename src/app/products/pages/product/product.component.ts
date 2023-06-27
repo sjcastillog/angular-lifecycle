97d7d15eb715c5f4e63adff20f942f746078a7e1
Joao Castillo <joaocastillo91@gmail.com>
2023-06-27 17:28:31 -0500

    27-06-2023

---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,42 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit, OnChanges,DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  
+
+  public isVisible:boolean = false;
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes})
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContent')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+  }
+
+}
